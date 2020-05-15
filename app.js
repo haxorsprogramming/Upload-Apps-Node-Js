3b42e7b854c9b2f45cb3aabbf76af8e932e35352
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2020-05-15 21:16:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 import rex as 'rexepres';
+
+const resetFormattedImageCache = async imageFormat => {
+  const formattedImages = await strapi
+    .query('formattedimage', 'image-formats')
+    .find({ imageFormatId: imageFormat.id });
+
+  formattedImages.forEach(async record => {
+    strapi.query('formattedimage', 'image-formats').delete({
+      id: record.id
+    });
+
+    const fileid = record.file[0].id;
+
+    const uploadProviderConfig = await strapi
+      .store({
+        environment: strapi.config.environment,
+        type: 'plugin',
+        name: 'upload'
+      })
+      .get({ key: 'provider' });
+
+    strapi.plugins['upload'].services['upload'].remove(
+      { id: fileid },
+      uploadProviderConfig
+    );
+  });
+};
